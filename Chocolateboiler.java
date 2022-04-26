b75dc7a494aca8d35d4fc0f0ae21ede54e92eadd
thiago gentil <thiagosgentil@gmail.com>
2022-04-26 20:03:09 -0300

    confirmar em 'master'

--- Chocolateboiler.java
+++ Chocolateboiler.java
@@ -14,4 +14,34 @@ public class Chocolateboiler {
 		 }
 		 return uniqueInstance;
 		}
-}
+		public void fill() { 
+			if (isEmpty()) {
+				empty = false;
+				boiled = false;
+				
+			}
+		}
+		
+		public void drain() {  
+			if(!isEmpty() && isBoiled()) {
+				
+				empty = true;
+			}
+		}
+		
+		public void boil(){ 
+			if (!isEmpty() && !isBoiled()) {
+				
+				boiled = true; 
+			}
+		}
+		
+		public boolean isEmpty() {
+			return empty;
+		}
+		
+		public boolean isBoiled() {
+			return boiled;
+		}
+	}
+		
